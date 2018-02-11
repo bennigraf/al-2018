ee6f4f94dc544ccfa3a428a438c8bf6b8e70cd54
Benjamin Graf <bennigraf@gmail.com>
2018-02-11 17:11:45 +0100

    Initial commit

---
+++ content/_index.md
@@ -0,0 +1,6 @@
+---
+title: Lineup
+layout: lineup
+---
+
+The lineup...
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp         = require("gulp"),
+    sass         = require("gulp-sass")
+
+// Compile SCSS files to CSS
+gulp.task("scss", function () {
+    gulp.src("themes/al-2018/static/scss/**/*.scss")
+        .pipe(sass({
+            // outputStyle : "compressed"
+        }))
+        // .pipe(autoprefixer({
+        //     browsers : ["last 20 versions"]
+        // }))
+        .pipe(gulp.dest("themes/al-2018/static/css"))
+})
+
+// Watch asset folder for changes
+gulp.task("watch", ["scss"], function () {
+    gulp.watch("themes/al-2018/static/scss/**/*", ["scss"])
+})
