0fb3469de14900af8448097a52c73156a2299752
Benjamin Graf <bennigraf@gmail.com>
2018-02-19 23:30:24 +0100

    Adding tap event for subnavi

--- themes/al-2018/static/js/main.js
+++ themes/al-2018/static/js/main.js
@@ -1,6 +1,8 @@
 $(function() {
-    $('a[title="Info"]').on('click', function(e) {
+    function toggleSubnav() {
         $(this).parent().find('ul').toggleClass('inactive');
         return false;
-    })
+    };
+    $('a[title="Info"]').on('click', toggleSubnav);
+    $('a[title="Info"]').on('tap', toggleSubnav);
 });
