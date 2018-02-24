db3e22d47149d9e2fbbcb8cfcc345c5253723a0b
Benjamin Graf <bennigraf@gmail.com>
2018-02-24 20:17:11 +0100

    bands POC

--- themes/al-2018/static/js/main.js
+++ themes/al-2018/static/js/main.js
@@ -4,4 +4,14 @@ $(function() {
         return false;
     };
     $('a[title="Info"]').on('click', toggleSubnav);
+
+    // activate overlay on click (for mobile)
+    $('.a-band').on('tap', function() {
+        if (!$(this).hasClass('hover')) {
+          $(this).addClass('hover');
+        } else {
+          var address = $(this).find('a').attr('href');
+          window.open(address, '_blank');
+        }
+      });
 });
