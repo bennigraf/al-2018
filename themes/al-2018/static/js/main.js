a8e7e584276e30baac1f1bfca70a9a2b4377b416
Benjamin Graf <bennigraf@gmail.com>
2018-02-26 23:09:08 +0100

    Adjust footer navigation

--- themes/al-2018/static/js/main.js
+++ themes/al-2018/static/js/main.js
@@ -1,17 +1,31 @@
 $(function() {
     function toggleSubnav() {
-        $(this).parent().find('ul').toggleClass('inactive');
+        var parent = $(this).parent();
+        var wasActive = !parent.find('ul').hasClass('inactive');
+        parent.parent().find('li > ul').addClass('inactive');
+        if (!wasActive) {
+            parent.find('ul').removeClass('inactive');
+        }
         return false;
     };
-    $('a[title="Info"]').on('click', toggleSubnav);
+    $('a[title="Info"], a[title="Rückblick"]').on('click', toggleSubnav);
 
     // activate overlay on click (for mobile)
+
+    // ToDo: Mobile detects both tap and click on the link (thus opens the link with the default 
+    //      click action). Find a way to only `.hover` on mobile and keep default functionality
+    //      (:hover) on desktop!
+    $('.a-band').on('click', function() {
+        console.log("clickclick");
+        return false;
+    });
     $('.a-band').on('tap', function() {
+        console.log("taptap");
         if (!$(this).hasClass('hover')) {
           $(this).addClass('hover');
         } else {
-          var address = $(this).find('a').attr('href');
-          window.open(address, '_blank');
+        //   var address = $(this).find('a').attr('href');
+        //   window.open(address, '_blank');
         }
       });
 });
