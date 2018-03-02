ecded1515dd0367e3a545b42facc630630fd9a27
Benjamin Graf <bennigraf@gmail.com>
2018-03-02 15:38:21 +0100

    Publishing some bands

--- themes/al-2018/static/js/main.js
+++ themes/al-2018/static/js/main.js
@@ -10,22 +10,11 @@ $(function() {
     };
     $('a[title="Info"], a[title="Rückblick"]').on('click', toggleSubnav);
 
-    // activate overlay on click (for mobile)
-
-    // ToDo: Mobile detects both tap and click on the link (thus opens the link with the default 
-    //      click action). Find a way to only `.hover` on mobile and keep default functionality
-    //      (:hover) on desktop!
+    // activate overlay on click
     $('.a-band').on('click', function() {
-        console.log("clickclick");
-        return false;
-    });
-    $('.a-band').on('tap', function() {
-        console.log("taptap");
         if (!$(this).hasClass('hover')) {
-          $(this).addClass('hover');
-        } else {
-        //   var address = $(this).find('a').attr('href');
-        //   window.open(address, '_blank');
+            $(this).addClass('hover');
+            return false;
         }
-      });
+    });
 });
